4f579d44a0552a5d3c94d09d1bb40d5d5c01863b
andredisa <andreadisanti22@gmail.com>
2023-11-16 15:56:44 +0100

    Invio posizioni dei giocatori al client

--- Server/src/ThreadGiocatore.java
+++ Server/src/ThreadGiocatore.java
@@ -1,50 +1,68 @@
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-class ThreadGiocatore extends Thread {
+public class ThreadGiocatore extends Thread {
     private Giocatore player;
-    private Socket socket;
     private Game game;
 
-    public ThreadGiocatore(Giocatore player, Socket socket, Game game) {
+    private DataInputStream dis;
+    private DataOutputStream dos;
+
+    public ThreadGiocatore(Giocatore player, Game game) {
         this.player = player;
-        this.socket = socket;
         this.game = game;
+        try {
+            dis = new DataInputStream(this.player.getSocket().getInputStream());
+            dos = new DataOutputStream(this.player.getSocket().getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void run() {
         try {
-            DataInputStream dis = new DataInputStream(socket.getInputStream());
-
             while (true) {
                 String command = dis.readUTF();
                 System.out.println("Coordinate ricevute: " + command);
 
-                // gestisci l'input del giocatore
                 String[] coordinates = command.split(";");
                 int newX = Integer.parseInt(coordinates[0]);
                 int newY = Integer.parseInt(coordinates[1]);
                 boolean b = Boolean.parseBoolean(coordinates[2]);
 
-                // Aggiorna la posizione del giocatore
+                // aggiorna la posizione del giocatore
                 game.movePlayer(player, newX, newY);
 
-                // controlli bomba
-
-                // aggiorna tutti i client delle posizioni degli altri giocatori
-
+                inviaPosizioniAiClient();
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            // Quando il giocatore si disconnette, rimuovilo dal gioco
             game.removePlayer(player);
             try {
-                socket.close();
+                player.getSocket().close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public void inviaPosizioniAiClient() {
+        try {
+            Messaggio m = new Messaggio("datiGiocatori");
+
+            for (Giocatore player : game.getPlayers()) {
+                m.aggiungiDato(player.toString());
+            }
+
+            ObjectOutputStream oos = new ObjectOutputStream(dos);
+            oos.writeObject(m);
+            oos.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }

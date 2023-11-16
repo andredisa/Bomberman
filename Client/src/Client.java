161a3fc2bd5b2ec1adedc2ee6cbe5c0aa03665d2
eabde <elmellalou@gmail.com>
2023-11-16 16:32:54 +0100

    Metodo riceviMessaggio dal server

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,24 +1,49 @@
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.Socket;
 
 public class Client {
     private static Socket socket;
+    private static DataInputStream dis;
+    private static DataOutputStream dos;
 
     public static void connect() {
         try {
             socket = new Socket("localhost", 5000);
-            System.out.println("Connesso al server.");
+            dis = new DataInputStream(socket.getInputStream());
+            dos = new DataOutputStream(socket.getOutputStream());
+            System.out.println("Connected to the server.");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void inviaCoordinate(int x, int y, boolean b) {
+    public static void inviaCoordinate(int x, int y, boolean bombaPiazzata) {
         try {
-            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-            dos.writeUTF(x + ";" + y + ";" + b);
+            dos.writeUTF(x + ";" + y + ";" + bombaPiazzata);
             dos.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Messaggio riceviMessaggio() {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            Messaggio messaggio = (Messaggio) ois.readObject();
+            System.out.println("Messaggio ricevuto: " + messaggio.toString());
+            return messaggio;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void closeConnection() {
+        try {
+            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }

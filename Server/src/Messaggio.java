e070f8a46a64c00421b78185ce0c2b3a45ca7af7
andredisa <andreadisanti22@gmail.com>
2023-11-16 15:25:24 +0100

    Classe messaggio per gestire la comunicazione client server

---
+++ Server/src/Messaggio.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Messaggio {
+    private String tipo;
+    private List<String> dati;
+
+    public Messaggio(String tipo) {
+        this.tipo = tipo;
+        this.dati = new ArrayList<>();
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void aggiungiDato(String d) {
+        this.dati.add(d);
+    }
+
+    public List<String> getDati() {
+        return dati;
+    }
+
+    public void setDati(List<String> dati) {
+        this.dati = dati;
+    }
+
+}

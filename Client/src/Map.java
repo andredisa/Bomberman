646b9ef7b3f9549e87edc75295a85c5c340c90cb
eabde <elmellalou@gmail.com>
2023-11-19 20:04:04 +0100

    Migliorata la gestione dei messaggi ricevuti e della grafica

--- Client/src/Map.java
+++ Client/src/Map.java
@@ -1,68 +1,70 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Map extends JPanel implements KeyListener {
 
-    private final int width = 17;
-    private final int height = 13;
-    private final int blockSize = 40;
-    private final String wallImage = "img/tile_wall.png";
-    private final String grassImage = "img/tile_grass.png";
-    private final String woodImage = "img/tile_wood.png";
+    public final static int WIDTH = 17;
+    public final static int HEIGHT = 13;
+    public final static int BLOCK_SIZE = 40;
+    public final static String WALL_IMAGE = "img/tile_wall.png";
+    public final static String GRASS_IMAGE = "img/tile_grass.png";
+    public final static String WOOD_IMAGE = "img/tile_wood.png";
+    public final static String BOMB_IMAGE = "img/bomb.png";
+    public final static String PLAYER_IMAGE = "img/player2.png";
 
-    private Image imgPlayer;
     private BufferedImage buffer;
-    private int playerX = 1;
-    private int playerY = 1;
+    public static int playerX = 1;
+    public static int playerY = 1;
     private int moveSpeed = 1;
+    boolean bombaPiazzata = false;
 
     private List<String> blocchiFissi = new ArrayList<>();
     private List<String> blocchiDistruttibili = new ArrayList<>();
     private List<String> giocatori = new ArrayList<>();
 
     public Map() {
-        setPreferredSize(new Dimension(width * blockSize + 1, height * blockSize + 1));
-        buffer = new BufferedImage(width * blockSize, height * blockSize, BufferedImage.TYPE_INT_ARGB);
-
-        try {
-            imgPlayer = ImageIO.read(Map.class.getResource("img/player2.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        setPreferredSize(new Dimension(WIDTH * BLOCK_SIZE + 1, HEIGHT * BLOCK_SIZE + 1));
+        buffer = new BufferedImage(WIDTH * BLOCK_SIZE, HEIGHT * BLOCK_SIZE, BufferedImage.TYPE_INT_ARGB);
 
         setFocusable(true);
         addKeyListener(this);
+
     }
 
     public void gestisciMessaggio() {
         try {
-            Messaggio ma = Client.riceviMessaggio();
+            while (true) {
+                Messaggio msg = Client.riceviMessaggio();
+                if (msg != null) {
+                    String messageType = msg.getTipo();
 
-            if (ma != null) {
-                String messageType = ma.getTipo();
-
-                switch (messageType) {
-                    case "blocchiFissi":
-                        blocchiFissi = ma.getDati();
-                        break;
-                    case "blocchiDistruttibili":
-                        blocchiDistruttibili = ma.getDati();
-                        break;
-                    case "datiGiocatori":
-                        giocatori = ma.getDati();
-                        break;
-                    default:
-                        System.out.println("Unhandled message type: " + messageType);
-                        break;
-                }
+                    switch (messageType) {
+                        case "blocchiFissi":
+                            blocchiFissi = msg.getDati();
+                            break;
+                        case "blocchiDistruttibili":
+                            blocchiDistruttibili = msg.getDati();
+                            break;
+                        case "datiGiocatori":
+                            giocatori = msg.getDati();
+                            break;
+                        default:
+                            System.out.println("Unhandled message type: " + messageType);
+                            break;
+                    }
+                } else
+                    break;
             }
+            ClientView.drawField(buffer.getGraphics());
+            ClientView.drawBlocks(buffer.getGraphics(), blocchiFissi, WALL_IMAGE);
+            ClientView.drawBlocks(buffer.getGraphics(), blocchiDistruttibili, WOOD_IMAGE);
+            ClientView.drawPlayers(buffer.getGraphics(), giocatori, PLAYER_IMAGE, BOMB_IMAGE);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -72,65 +74,7 @@ public class Map extends JPanel implements KeyListener {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         gestisciMessaggio();
-        disegnaCampo(buffer.getGraphics());
-        disegnaBlocchi(buffer.getGraphics(), blocchiFissi, wallImage);
-        disegnaBlocchi(buffer.getGraphics(), blocchiDistruttibili, woodImage);
-        disegnaGiocatori(buffer.getGraphics(), giocatori);
-
         g.drawImage(buffer, 0, 0, this);
-    }
-
-    public void disegnaCampo(Graphics g) {
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                drawBlock(g, i, j, grassImage);
-            }
-        }
-    }
-
-    public void drawBlock(Graphics g, int x, int y, String imagePath) {
-        try {
-            Image blockImage = ImageIO.read(Map.class.getResource(imagePath));
-            g.drawImage(blockImage, x * blockSize, y * blockSize, blockSize, blockSize, null);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-     public void disegnaGiocatori(Graphics g, List<String> giocatori) {
-        try {
-            for (String s : giocatori) {
-                String[] blockData = s.split(";");
-                if (blockData.length == 3) {
-                    int x = Integer.parseInt(blockData[0].trim());
-                    int y = Integer.parseInt(blockData[1].trim());
-                    boolean b = Boolean.parseBoolean(blockData[2].trim());
-                    disegnaPlayer(g, x, y);
-                    if (b) {
-                        drawBlock(g, x, y, "img/bomb.png");
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    public void disegnaBlocchi(Graphics g, List<String> blocchi, String imagePath) {
-        try {
-            for (String s : blocchi) {
-                String[] blockData = s.split(";");
-                if (blockData.length == 3) {
-                    int x = Integer.parseInt(blockData[0].trim());
-                    int y = Integer.parseInt(blockData[1].trim());
-                    drawBlock(g, x, y, imagePath);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void disegnaPlayer(Graphics g , int x , int y) {
-        g.drawImage(imgPlayer, x * blockSize, y * blockSize, blockSize, blockSize, null);
     }
 
     @Override
@@ -138,7 +82,7 @@ public class Map extends JPanel implements KeyListener {
         int keyCode = e.getKeyCode();
         int newX = playerX;
         int newY = playerY;
-        boolean bombaPiazzata = false;
+
         switch (keyCode) {
             case KeyEvent.VK_UP:
                 newY -= moveSpeed;
@@ -156,8 +100,8 @@ public class Map extends JPanel implements KeyListener {
                 bombaPiazzata = true;
                 break;
         }
-        playerX = newX;
-        playerY = newY;
+        // playerX = newX;
+        // playerY = newY;
 
         Client.inviaCoordinate(newX, newY, bombaPiazzata);
         repaint();
@@ -165,11 +109,10 @@ public class Map extends JPanel implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
-        // Add any necessary code for key released events
+        bombaPiazzata = false;
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
-        // Add any necessary code for key typed events
     }
 }

b26e92796fe87edc20b1a7582773d18dc6557122
eabde <elmellalou@gmail.com>
2023-11-26 19:27:45 +0100

    -Modifica metodo per ricevere i dati dal server

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,20 +1,19 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
 
 public class Client {
+    private static final String SERVER_IP = "localhost";
+    private static final int SERVER_PORT = 5000;
+
     private static Socket socket;
     private static DataInputStream dis;
     private static DataOutputStream dos;
 
     public static void connect() {
         try {
-            // ip = 172.16.102.115
-            socket = new Socket("localhost", 5000);
+            socket = new Socket(SERVER_IP, SERVER_PORT);
             dis = new DataInputStream(socket.getInputStream());
             dos = new DataOutputStream(socket.getOutputStream());
             System.out.println("Connected to the server.");
@@ -23,28 +22,42 @@ public class Client {
         }
     }
 
-    public static void inviaCoordinate(int x, int y, boolean bombaPiazzata) {
+    public static void sendCoordinates(int x, int y, boolean bombPlaced) {
         try {
-            dos.writeUTF(x + ";" + y + ";" + bombaPiazzata);
+            dos.writeUTF(x + ";" + y + ";" + bombPlaced);
             dos.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static Messaggio riceviMessaggio() throws SocketException {
+    public static Messaggio receiveMessage() {
         try {
             socket.setSoTimeout(2000);
-            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-            Messaggio messaggio = (Messaggio) ois.readObject();
 
-            System.out.println("Messaggio ricevuto: " + messaggio.getTipo());
-            return messaggio;
+            // Verifica se ci sono dati disponibili prima di leggere
+            if (dis.available() > 0) {
+                int dataSize = dis.readInt();
+                byte[] data = new byte[dataSize];
+                dis.readFully(data);
+
+                try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data))) {
+                    Messaggio message = (Messaggio) ois.readObject();
+                    System.out.println("Messaggio ricevuto: " + message.getTipo());
+                    return message;
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
         } catch (SocketTimeoutException e) {
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            socket.setSoTimeout(0);
+            try {
+                socket.setSoTimeout(0);
+            } catch (SocketException e) {
+                e.printStackTrace();
+            }
         }
         return null;
     }
